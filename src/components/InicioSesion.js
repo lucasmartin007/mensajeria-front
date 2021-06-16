10452780205c8abfafb9de412ecf149318bd86a1
lucasmartin007 <luesma102938475@hotmail.com>
2021-06-16 11:34:09 -0300

    Inicio de sesion

--- src/components/InicioSesion.js
+++ src/components/InicioSesion.js
@@ -4,19 +4,17 @@ import { useState, useEffect } from "react";
 
     
 export const InicioSesion = () => {
-    let [userEmail, setUserEmail] = useState("");
+    let [userUsername, setUserUsername] = useState("");
     let [userPassword, setUserPassword] = useState("");
 
-    const onUserEmailChange = e => setUserEmail(e.target.value);
+    const onUserUsernameChange = e => setUserUsername(e.target.value);
     const onUserPasswordChange = e => setUserPassword(e.target.value);
-
-    //
 
     const handleSubmitLogin = e => {
         e.preventDefault();
     
         const data = {
-            "email": userEmail,
+            "username": userUsername,
             "password": userPassword,   };
         const requestOptions = {
           method: "POST",
@@ -24,8 +22,8 @@ export const InicioSesion = () => {
           body: JSON.stringify(data)
         };
         fetch("http://localhost:3000/usuarios/login", requestOptions) // "https://jsonplaceholder.typicode.com/posts"
-        .then(response => response.json())      
-        .then(res => console.log(res));  
+        .then(response => response.json())
+        .then(res => console.log(res))
     };
 
 //     useEffect(()=>{
@@ -41,7 +39,7 @@ export const InicioSesion = () => {
         <h2>Iniciar sesion</h2>
         {/* <form> */}
             Email de usuario<br />
-            <input type="text" value={userEmail} onChange={onUserEmailChange} /><br /><br />
+            <input type="text" value={userUsername} onChange={onUserUsernameChange} /><br /><br />
             Contraseña<br />
             <input type="password" value={userPassword} onChange={onUserPasswordChange} /><br /><br />
             <button onClick = {handleSubmitLogin}>Iniciar sesion</button><br /><br /> {/* */}
